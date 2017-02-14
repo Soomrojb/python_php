91dc74a8dd751987dfde2afd8186439e8c4fcedd
Janib Soomro <Soomrojb@gmail.com>
2017-02-14 06:51:56 +0000

    Print infromation at browser

--- py_cron.php
+++ py_cron.php
@@ -7,6 +7,7 @@
 		@developer:		Janib Soomro
 		@contact:		soomrojb@gmail.com | skype: soomrojb
 		@date:			10th Feb. 2017
+		@updated:		14th Feb. 2017
 		
 	*/
 
@@ -16,20 +17,40 @@
 	
 	//	Target website pool
 	$CmdQueue	=	array(
-						"olx.pk" => array(	" --site olxpk --depth 2 --url https://www.olx.com.pk/karachi/computers-accessories/",
-											" --site olxpk --depth 0 --url https://www.olx.com.pk/karachi/games-entertainment/")
+						"olx.pk" => array(	" --site olxpk --depth 2 --url https://www.olx.com.pk/karachi/computers-accessories/"
+											//" --site olxpk --depth 0 --url https://www.olx.com.pk/karachi/games-entertainment/"
+										)
 					);
 	
 	//	Build command(s) for Python
 	foreach($CmdQueue as $Key => $Value) {
 		foreach($Value as $FinalVal) {
 			$NewCmd		=	$PyExec . " " . $CrawlSrc . " " . $FinalVal;
-			echo	'<br/>' . $NewCmd . '<br/>';
+			//echo	'<br/>' . $NewCmd . '<br/>';
 			$JSonData	=	shell_exec($NewCmd);
 			if ($JSonData) {
-				echo	'<br/>';
-				echo	$JSonData;
-				echo	'<br/>';
+				//	print raw response
+				//echo	$JSonData;
+				
+				//	decode JSON and print
+				$jContent	=	json_decode($JSonData, true);
+				echo	"<table><tbody>";
+				foreach ($jContent as $key => $value) {
+					$category	=	$value['Category'];
+					$price		=	$value['Price'];
+					$thumbnail	=	$value['Thumb'];
+					$location	=	$value['Location'];
+					$title		=	$value['Title'];
+					
+					echo	"<tr>";
+					echo	"	<td><img src='$thumbnail' /></td>";
+					echo	"	<td>$title</td>";
+					echo	"	<td>$price</td>";
+					echo	"	<td>$category</td>";
+					echo	"	<td>$location</td>";
+					echo	"<tr>";
+				}
+				echo	"</tbody></table>";
 			}
 		}
 	}
