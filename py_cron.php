0b4a520722a63e2eefceff670e57c7615d8492f6
Janib Soomro <Soomrojb@gmail.com>
2017-02-14 07:05:37 +0000

    Printing data with 'bootstrap'

--- py_cron.php
+++ py_cron.php
@@ -17,10 +17,9 @@
 	
 	//	Target website pool
 	$CmdQueue	=	array(
-						"olx.pk" => array(	" --site olxpk --depth 2 --url https://www.olx.com.pk/karachi/computers-accessories/"
-											//" --site olxpk --depth 0 --url https://www.olx.com.pk/karachi/games-entertainment/"
-										)
-					);
+					"olx.pk" => array(	" --site olxpk --depth 10 --url https://www.olx.com.pk/karachi/computers-accessories/"
+										//" --site olxpk --depth 0 --url https://www.olx.com.pk/karachi/games-entertainment/"
+					));
 	
 	//	Build command(s) for Python
 	foreach($CmdQueue as $Key => $Value) {
@@ -34,7 +33,23 @@
 				
 				//	decode JSON and print
 				$jContent	=	json_decode($JSonData, true);
-				echo	"<table><tbody>";
+				echo	'<meta charset="utf-8">';
+				echo	'<meta name="viewport" content="width=device-width, initial-scale=1">';
+				echo	'<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">';
+				echo	'<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>';
+				echo	'<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>';
+				echo	"<div class='container'>";
+				echo	"<table class='table table-hover'>";
+				echo	"<thead>";
+				echo	"	<tr>";
+				echo	"		<td>Picture</td>";
+				echo	"		<td>Title</td>";
+				echo	"		<td>Price</td>";
+				echo	"		<td>Category</td>";
+				echo	"		<td>Location</td>";
+				echo	"	</tr>";
+				echo	"</thead>";
+				echo	"<tbody>";
 				foreach ($jContent as $key => $value) {
 					$category	=	$value['Category'];
 					$price		=	$value['Price'];
@@ -43,7 +58,7 @@
 					$title		=	$value['Title'];
 					
 					echo	"<tr>";
-					echo	"	<td><img src='$thumbnail' /></td>";
+					echo	"	<td><img src='$thumbnail' class='img-thumbnail' width='100' height='100'></td>";
 					echo	"	<td>$title</td>";
 					echo	"	<td>$price</td>";
 					echo	"	<td>$category</td>";
@@ -51,6 +66,7 @@
 					echo	"<tr>";
 				}
 				echo	"</tbody></table>";
+				echo	"</div>";
 			}
 		}
 	}
